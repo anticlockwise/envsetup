bc80ed1a6831e0a9c269cb876858f139f58fe756
Rongzhou Shen <anticlockwise5@gmail.com>
2014-12-22 10:46:29 -0800

    Adding setup for powerline fonts

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -71,6 +71,13 @@ if [ $answer == "y" ]; then
     git clone git://github.com/altercation/solarized.git
 
     echo "Setting up powerline"
+    wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
+    wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+    mkdir ~/.fonts/
+    mv PowerlineSymbols.otf ~/.fonts/
+    fc-cache -vf ~/.fonts/
+    mkdir -p ~/.config/fontconfig/conf.d/
+    mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
     cp -r configs/powerline ~/.config/powerline
 
     echo "Setting up mutt with solarized colorscheme"
