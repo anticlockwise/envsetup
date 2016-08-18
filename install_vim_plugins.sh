5f138afa0e700587160fcfb7861da8be578445fc
Rongzhou Shen <rongzhou@amazon.com>
2016-08-18 17:08:01 +0000

    Using Vundle to install Vim plugins instead

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -1,25 +1,9 @@
-echo "Installing Pathogen for VIM"
-mkdir -p ~/.vim/autoload
+echo "Installing Vundle for VIM"
 mkdir -p ~/.vim/bundle
 
-git clone https://github.com/tpope/vim-pathogen.git
-cp ./vim-pathogen/autoload/pathogen.vim ~/.vim/autoload/
-rm -rf ./vim-pathogen
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo "Downloading VIM bundles"
+cp configs/vimrc ~/.vimrc
 
-CUR_DIR=`pwd`
-cd ~/.vim/bundle
-
-for plugin in `cat vim_plugins`; do
-    git clone $plugin
-done
-
-cd $CUR_DIR
-cp ./configs/vimrc ~/.vimrc
-
-mkdir -p ~/.vim/syntax
-mkdir -p ~/.vim/ftplugin
-cp ./vim/django.vim ~/.vim/syntax/
-cp ./vim/htmldjango.vim ~/.vim/syntax/
-cp ./vim/ftplugin_django.vim ~/.vim/ftplugin/
+echo "Installing Vim Plugins"
+vim +PluginInstall +qall
